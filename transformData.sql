7a50e0080388e65a518c149b9ac4b64e18970f4b
ryan <ryanba08@icloud.com>
2024-05-28 22:45:07 -0400

    added min max salary

--- transformData.sql
+++ transformData.sql
@@ -31,23 +31,40 @@ FROM job_info;
 ALTER TABLE job_info ADD COLUMN posted_on DATE;
 UPDATE job_info
 SET posted_on = CASE
-    WHEN LOWER(date_posted) LIKE '%today%' THEN CURRENT_DATE()
-    WHEN LOWER(date_posted) LIKE '%minute%' THEN DATEADD(minute, -IFF(NULLIF(REGEXP_REPLACE(date_posted, '[^0-9]', ''), '') IS NOT NULL, TO_NUMBER(REGEXP_REPLACE(date_posted, '[^0-9]', '')), 0), CURRENT_DATE())
-    WHEN LOWER(date_posted) LIKE '%hour%' THEN DATEADD(hour, -IFF(NULLIF(REGEXP_REPLACE(date_posted, '[^0-9]', ''), '') IS NOT NULL, TO_NUMBER(REGEXP_REPLACE(date_posted, '[^0-9]', '')), 0), CURRENT_DATE())
-    WHEN LOWER(date_posted) LIKE '%day%' THEN DATEADD(day, -IFF(NULLIF(REGEXP_REPLACE(date_posted, '[^0-9]', ''), '') IS NOT NULL, TO_NUMBER(REGEXP_REPLACE(date_posted, '[^0-9]', '')), 0), CURRENT_DATE())
+    WHEN date_posted ILIKE '%today%' THEN CURRENT_DATE()
+    WHEN date_posted ILIKE '%minute%' THEN DATEADD(minute, -IFF(NULLIF(REGEXP_REPLACE(date_posted, '[^0-9]', ''), '') IS NOT NULL, TO_NUMBER(REGEXP_REPLACE(date_posted, '[^0-9]', '')), 0), CURRENT_DATE())
+    WHEN date_posted ILIKE '%hour%' THEN DATEADD(hour, -IFF(NULLIF(REGEXP_REPLACE(date_posted, '[^0-9]', ''), '') IS NOT NULL, TO_NUMBER(REGEXP_REPLACE(date_posted, '[^0-9]', '')), 0), CURRENT_DATE())
+    WHEN date_posted ILIKE '%day%' THEN DATEADD(day, -IFF(NULLIF(REGEXP_REPLACE(date_posted, '[^0-9]', ''), '') IS NOT NULL, TO_NUMBER(REGEXP_REPLACE(date_posted, '[^0-9]', '')), 0), CURRENT_DATE())
     ELSE NULL
 END;
-SELECT *
-FROM job_info
-LIMIT 10;
 
-SELECT
-    CASE
-        WHEN salary LIKE '%$%' THEN REPLACE(REPLACE(salary, '$', ''), ',', '')
-        ELSE salary
-    END AS cleaned_salary
+ALTER TABLE job_info
+ADD COLUMN cleaned_salary TEXT;
+UPDATE job_info
+SET cleaned_salary = REGEXP_REPLACE(cleaned_salary, '[^0-9-]', '');
+SELECT salary,cleaned_salary
 FROM job_info
-LIMIT 10;
-SELECT *
-FROM JOB_INFO
-limit 10;
+LIMIT 100;
+
+ALTER TABLE job_info
+ADD COLUMN IF NOT EXISTS min_salary NUMBER;
+ALTER TABLE job_info
+ADD COLUMN IF NOT EXISTS max_salary NUMBER;
+UPDATE job_info
+SET min_salary =
+    CASE
+        WHEN salary ILIKE '%annually%' THEN TRY_TO_NUMBER(TRIM(SPLIT_PART(cleaned_salary, '-', 1))) * 1000
+        WHEN salary ILIKE '%hour' THEN TRY_TO_NUMBER(TRIM(SPLIT_PART(cleaned_salary, '-', 1))) * 40 * 52
+        WHEN salary ILIKE '%year%' THEN TRY_TO_NUMBER(SPLIT_PART(cleaned_salary, '-', 1))
+        ELSE NULL
+        END,
+    max_salary =
+    CASE
+        WHEN salary ILIKE '%annually%' THEN TRY_TO_NUMBER(TRIM(SPLIT_PART(cleaned_salary, '-', 2))) * 1000
+        WHEN salary ILIKE '%hour' THEN TRY_TO_NUMBER(TRIM(SPLIT_PART(cleaned_salary, '-', 2))) * 40 * 52
+        WHEN salary ILIKE '%year%' THEN TRY_TO_NUMBER(SPLIT_PART(cleaned_salary, '-', 2))
+    END;
+SELECT COUNT(*) as row_count
+FROM job_info
+
+WHERE min_salary is NULL;
